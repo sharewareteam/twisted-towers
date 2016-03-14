8ea325c0a51839082a5550f6520d200ad6776ae7
João Maia <joao.maia@fabamaq.com>
2016-03-14 12:31:56 +0000

    Minor fix.

--- js/entities.js
+++ js/entities.js
@@ -79,7 +79,7 @@ function Enemy(x, y, type) {
   enemy.animations.add("up", [90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119], 30, true);
 
   enemy.state = game.state.getCurrentState();
-  findPathTo((x/TILEWIDTH)|0,(y/TILEHEIGHT)|0,state.endTile.x,state.endTile.y, (function(path) {
+  findPathTo((x/TILEWIDTH)|0,(y/TILEHEIGHT)|0,enemy.state.endTile.x,enemy.state.endTile.y, (function(path) {
     this.path = path;
   }).bind(enemy));
 
